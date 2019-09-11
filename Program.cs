0bcfaac5e3c1c0228d7dc13bc25844091809ee8e
张大嘴 <yibao16@163.com>
2019-09-11 10:44:16 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StartupManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ StartupModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StartupManagement
+{
+    class StartupModel
+    {
+        public int ID { get; set; }
+        public string ProcessName { get; set; }
+        public string Path { get; set; }
+    }
+}
